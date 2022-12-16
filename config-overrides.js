15d4d91a5d29affcad5b7741378b6d8f30da70ac
Lisa-Ann B <lisabruney@pitt.edu>
2022-12-16 16:11:19 -0500

    Delete unused files

--- config-overrides.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-module.exports = {
-    paths: function (paths, env) {
-        console.log(paths)
-        paths.appPublic = path.resolve(__dirname, 'docs');
-        paths.appHtml = path.resolve(__dirname, 'src/index.html');
-        return paths;
-    }
-}
