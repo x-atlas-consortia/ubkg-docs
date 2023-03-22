bcf1817a7345927c3cfda597684097d56c2a168c
Lisa-Ann B <lisabruney@pitt.edu>
2023-03-22 09:19:43 -0400

    Rebuild main.css

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,8 +1,8 @@
 /**
- * sennetdocs - 
+ * ubkg-docs - 
  * @version v0.1.0
- * @link https://docs.sennetconsortium.org/
- * @date Thu Mar 16 2023 10:28:11 GMT-0400 (Eastern Daylight Time)
+ * @link https://ubkg.docs.xconsortia.org/
+ * @date Tue Mar 21 2023 18:08:36 GMT-0400 (Eastern Daylight Time)
  */
 var _this10 = this;
 function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); enumerableOnly && (symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; })), keys.push.apply(keys, symbols); } return keys; }
